ad65e8b9ade5a134342fa3d64ac9697771567cc4
Timothy <tmeehan26@gmail.com>
2025-06-26 01:19:30 -0400

    added output folder

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 export default {
     input: 'weave-3d.js',
     output: {
-      file: '../weave-3d.esm.js',
+      file: 'output/weave-3d.esm.js',
       format: 'es',
       sourcemap: true
     }
